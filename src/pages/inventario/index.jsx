87fc522d58e4d104ebd3a275923e386a2e654b35
DilbertRV <gearxdtrece@gmail.com>
2023-01-19 01:58:43 -0600

    Delete product, Update TableMenu Client Side

--- src/pages/inventario/index.jsx
+++ src/pages/inventario/index.jsx
@@ -1,18 +1,54 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { TableMenu } from "../../components/TableMenu";
-import { Table, Button, Modal, TextInput, Label } from "flowbite-react";
+import { Button } from "flowbite-react";
+import { ToastMensaje } from "components/ToastMensaje";
+import { useEffect } from "react";
 
 Inventario.titulo = "Inventario";
 export default function Inventario({ menus }) {
   const router = useRouter();
   const [selectedMenu, setSelectedMenu] = useState(menus[0]);
-  const [orden, setOrden] = useState([]);
+
+  useEffect(() => {
+    setSelectedMenu(menus[0]);
+  }, [menus]);
 
   const handleSelectedMenu = (e) => {
     if (e.target.textContent === selectedMenu.nombre) return;
     const menu = menus.find((menu) => menu.nombre === e.target.textContent);
     setSelectedMenu(menu);
+  };
+
+  const eliminarProducto = async (menu_id, categoria_id, producto_id) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/inventario/${menu_id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            menu_id: menu_id,
+            categoria_id: categoria_id,
+            producto_id: producto_id,
+          }),
+        }
+      );
+      if (response.status === 200) {
+        router.push(
+          {
+            pathname: "/inventario",
+            query: {
+              mensaje: `${producto_id}`,
+              type: "Eliminar",
+            },
+          },
+          "/inventario"
+        );
+      }
+    } catch (error) {}
   };
 
   if (menus.length === 0)
@@ -25,13 +61,24 @@ export default function Inventario({ menus }) {
       </div>
     );
   return (
-    <TableMenu
-      menus={menus}
-      handleSelectedMenu={handleSelectedMenu}
-      selectedMenu={selectedMenu}
-      orden={orden}
-      type="inventario"
-    />
+    <>
+      {router.query.mensaje ? (
+        <ToastMensaje mensaje={router.query.mensaje} type={router.query.type} />
+      ) : null}
+      <Button
+        className="absolute right-0 top-56 bg-red-500 hover:bg-red-600 rounded-r-none w-52 h-[65px] shadow-lg"
+        onClick={() => router.push("inventario/new")}
+      >
+        Agregar producto
+      </Button>
+      <TableMenu
+        menus={menus}
+        handleSelectedMenu={handleSelectedMenu}
+        selectedMenu={selectedMenu}
+        eliminarProducto={eliminarProducto}
+        type="inventario"
+      />
+    </>
   );
 }
 
